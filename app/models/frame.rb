7ac2e6b4a486a388f5140abff9c30d32955da516
tzanarde <zanarde.tiago@gmail.com>
2025-10-19 11:10:11 -0300

    feat: frame model with references and validations

---
+++ app/models/frame.rb
@@ -0,0 +1,17 @@
+class Frame < ApplicationRecord
+  # References
+  has_many :circles
+
+  # Validations
+  validates :center_x, presence: true,
+                       numericality: { greater_than_or_equal_to: 0 }
+
+  validates :center_y, presence: true,
+                       numericality: { greater_than_or_equal_to: 0 }
+
+  validates :height, presence: true,
+                     numericality: { greater_than_or_equal_to: 0 }
+
+  validates :width, presence: true,
+                    numericality: { greater_than_or_equal_to: 0 }
+end
