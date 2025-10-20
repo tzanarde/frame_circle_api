8b532e28f8bc6ab03cdfbd429d8112b3a1c1c753
tzanarde <zanarde.tiago@gmail.com>
2025-10-20 17:46:11 -0300

    refact: frame model

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -9,9 +9,9 @@ class Frame < ApplicationRecord
   validates :center_y, presence: true,
                        numericality: { greater_than_or_equal_to: 0 }
 
-  validates :height, presence: true,
-                     numericality: { greater_than_or_equal_to: 0 }
-
   validates :width, presence: true,
                     numericality: { greater_than_or_equal_to: 0 }
+
+  validates :height, presence: true,
+                     numericality: { greater_than_or_equal_to: 0 }
 end
