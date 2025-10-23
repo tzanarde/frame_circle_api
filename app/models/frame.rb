ea78d62a9327e8f3109fc7d3dca5f928ca73a406
tzanarde <zanarde.tiago@gmail.com>
2025-10-22 21:24:59 -0300

    feat: frame model

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -1,17 +1,33 @@
 class Frame < ApplicationRecord
-  # References
-  has_many :circles
+  # Associations
+  has_many :circles, dependent: :destroy
 
   # Validations
-  validates :center_x, presence: true,
-                       numericality: { greater_than_or_equal_to: 0 }
+  validates :center_x, :center_y, :width, :height,
+            presence: true,
+            numericality: { greater_than_or_equal_to: 0 }
 
-  validates :center_y, presence: true,
-                       numericality: { greater_than_or_equal_to: 0 }
+  # Cutom Validations
+  validate :must_not_overlap_or_touch_other_frames, if: :any_frames?
 
-  validates :width, presence: true,
-                    numericality: { greater_than_or_equal_to: 0 }
+  # Scopes
+  scope :frames_overlapping, -> (center_x, center_y, width, height, exclude_id) do
+    where('ABS(center_x - ?) <= ((width / 2.0) + (? / 2.0))', center_x, width)
+    .where('ABS(center_y - ?) <= ((height / 2.0) + (? / 2.0))', center_y, height)
+    .where.not(id: exclude_id)
+  end
 
-  validates :height, presence: true,
-                     numericality: { greater_than_or_equal_to: 0 }
+  private
+
+  def must_not_overlap_or_touch_other_frames
+    return unless frame_overlaps_exist?
+    
+    errors.add(:base, I18n.t('messages.error.frame_overlap_or_touch_an_existing_frame'))
+  end
+
+  def frame_overlaps_exist?
+    self.class.frames_overlapping(center_x, center_y, width, height, id).exists?
+  end
+
+  def any_frames? = self.class.exists?
 end
