c965521a77d0f2fa400fd6ca08d07da821fb8819
tzanarde <zanarde.tiago@gmail.com>
2025-10-27 18:56:57 -0300

    config: log config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -50,4 +50,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.logger = ActiveSupport::Logger.new(Rails.root.join("log/test.log"))
+  config.logger.formatter = ::Logger::Formatter.new
+  config.log_level = :error
 end
